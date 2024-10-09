8b810b850462ca41de4310c5b4a0e3b62a033a5b
Михаил Будылин <bmichailabc@gmail.com>
2024-10-09 20:46:37 +0300

    Сделан светофор на 3х светодиодах

--- src/led.h
+++ src/led.h
@@ -1,16 +1,91 @@
+class LED{
+public:
+
+    LED(int a_pin): pin{a_pin} {};
+
+    void on(){digitalWrite(pin, HIGH);}
+    
+    void off(){digitalWrite(pin, LOW);}
+
+    void init(){pinMode(pin, OUTPUT);}
+
+    void on_time_repeat(int time, int repeat){   // Включает светодиод на определённое время time определённое число раз repeat
+    int i = repeat;
+    while (i){
+        i-=1;
+        delay(time);
+        on();
+        delay(time);
+        off();
+        }
+}
+private:
+    int pin;
+};
+
+class Digital_indicator{
+public:
+    Digital_indicator(int up, int average, int down, int l_up, int l_down, int r_up, int r_down){
+        pin_up = up; 
+        pin_average = average; 
+        pin_down = down; 
+        pin_l_up = l_up; 
+        pin_l_down = l_down; 
+        pin_r_up = r_up; 
+        pin_r_down = r_down;
+    };
+    void init(){
+        pinMode(pin_up, OUTPUT);
+        pinMode(pin_average, OUTPUT);
+        pinMode(pin_down, OUTPUT);
+        pinMode(pin_l_up, OUTPUT);
+        pinMode(pin_l_down, OUTPUT);
+        pinMode(pin_r_up, OUTPUT);
+        pinMode(pin_r_down, OUTPUT);
+        }
+    void off(){
+        digitalWrite(pin_up, HIGH);
+        digitalWrite(pin_average, HIGH);
+        digitalWrite(pin_down, HIGH);
+        digitalWrite(pin_l_down, HIGH);
+        digitalWrite(pin_l_up, HIGH);
+        digitalWrite(pin_r_down, HIGH);
+        digitalWrite(pin_r_up, HIGH);
+    }
+    void on(){
+        digitalWrite(pin_up, HIGH);
+        digitalWrite(pin_average, HIGH);
+        digitalWrite(pin_down, HIGH);
+        digitalWrite(pin_l_down, HIGH);
+        digitalWrite(pin_l_up, HIGH);
+        digitalWrite(pin_r_down, HIGH);
+        digitalWrite(pin_r_up, HIGH);
+    }
+
+private:
+    int pin_up; 
+    int pin_average; 
+    int pin_down; 
+    int pin_l_up; 
+    int pin_l_down; 
+    int pin_r_up; 
+    int pin_r_down;
+};
+
 namespace Builtin_led{
 
+constexpr int LED_PIN{13};
+
 void on(){
-    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_PIN, HIGH);
 }
 void off(){
-    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_PIN, LOW);
 }
 void init(){
-    pinMode(LED_BUILTIN, OUTPUT);
-}}
-
-void led_time_repeat(int time, int repeat){   // Включает светодиод на определённое время time определённое число раз repeat
+    pinMode(LED_PIN, OUTPUT);
+}
+void on_time_repeat(int time, int repeat){   // Включает светодиод на определённое время time определённое число раз repeat
     int i = repeat;
     while (i){
         i-=1;
@@ -20,18 +95,41 @@ void led_time_repeat(int time, int repeat){   // Включает светоди
         Builtin_led::off();
         }
 }
+}
 
+namespace Led{
+void init(int pin){
+    pinMode(pin, OUTPUT);
+}
+void on(int pin){
+    digitalWrite(pin, HIGH);
+}
+void off(int pin){
+    digitalWrite(pin, LOW);
+}
+void on_time_repeat(int pin, int time, int repeat){   // Включает светодиод на определённое время time определённое число раз repeat
+    int i = repeat;
+    while (i){
+        i-=1;
+        delay(time);
+        Led::on(pin);
+        delay(time);
+        Led::off(pin);
+        }
+}
+}
 
 void delay_s(int i){
     delay(i*1000);
 }
 
 /*void led_on_time (int time){
-    int duty = 0;
+    int duty = 1;
     time = time / 100;
     int timer = millis();
     int timer2 = 0;
     while (duty != 100){
+
         duty+=1;
         timer2 = millis();
         while ((millis() - timer) < time){ 
--- src/src.ino
+++ src/src.ino
@@ -3,14 +3,36 @@
 constexpr unsigned int PERIOD{2};
 constexpr unsigned int HALF_PERIOD{PERIOD / 2};
 
+unsigned int a = 1;
+LED red = 13;
+LED yellow = 12;
+LED green = 11;
+
+
 void setup(){
-    Builtin_led::init();
+    red.init();
+    yellow.init();
+    green.init();
+    Serial.begin(115200);
 }
 
 void loop(){
-    Builtin_led::on();
-    delay_s(HALF_PERIOD);
-    Builtin_led::off();
-    delay_s(HALF_PERIOD);
+    red.on();
+    delay(5000);
+    yellow.on();
+    Serial.println("Ready!");
+    delay(1000);
+    yellow.off();
+    red.off();
+    green.on();
+    Serial.println("GO!");
+    delay(5000);
+    green.off();
+    green.on_time_repeat(500, 3);
+    Serial.println("Ready to stop!");
+    yellow.on();
+    delay(1500);
+    yellow.off();
+    Serial.println("STOP!");
 
     }
